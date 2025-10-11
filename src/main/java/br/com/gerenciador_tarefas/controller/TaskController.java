cc59d4f23327ff9541f77df798a098e52595ba69
IgorPintoS <igorpintosantos05@gmail.com>
2025-10-10 23:06:06 -0300

    terceiro commit, utilização do JPARepository, adequação dos métodos findAll e saveTask, e classe Task.

--- src/main/java/br/com/gerenciador_tarefas/controller/TaskController.java
+++ src/main/java/br/com/gerenciador_tarefas/controller/TaskController.java
@@ -1,6 +1,7 @@
 package br.com.gerenciador_tarefas.controller;
 
 import br.com.gerenciador_tarefas.model.Task;
+import br.com.gerenciador_tarefas.repository.TaskRepository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -11,18 +12,47 @@ import java.util.concurrent.atomic.AtomicLong;
 @RequestMapping("/api/tasks")
 public class TaskController {
 
-    private final List<Task> taskList = new ArrayList<>();
-    private final AtomicLong counter = new AtomicLong();
+    private final TaskRepository taskRepository;
 
-    @GetMapping
-    public List<Task> getTaskList() {
-        return taskList;
+//    private final List<Task> taskList = new ArrayList<>();
+//    private final AtomicLong counter = new AtomicLong();
+
+    public TaskController(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
     }
 
+//    @GetMapping
+//    public List<Task> getTaskList() {
+//        return taskList;
+//    }
+//
+//    @PostMapping
+//    public Task createTask(@RequestBody Task task) {
+//        task.setId(counter.getAndIncrement());
+//        taskList.add(task);
+//        return task;
+//    }
+    @GetMapping
+    public List<Task> findAll() {
+        List<Task> taskList;
+        try {
+            taskList = taskRepository.findAll();
+        } catch (NullPointerException e) {
+            e.getStackTrace();
+            return null;
+        }
+        return taskList;
+    }
     @PostMapping
-    public Task createTask(@RequestBody Task task) {
-        task.setId(counter.getAndIncrement());
-        taskList.add(task);
-        return task;
+    public void saveTask(@RequestBody Task task) {
+        try {
+            task.setDescription(task.getDescription());
+            task.setPriority(task.getPriority());
+            taskRepository.save(task);
+        } catch (NullPointerException e) {
+            e.getStackTrace();
+        }
+
+        System.out.println("Task createad.");
     }
 }
--- src/main/java/br/com/gerenciador_tarefas/model/Task.java
+++ src/main/java/br/com/gerenciador_tarefas/model/Task.java
@@ -3,8 +3,9 @@ package br.com.gerenciador_tarefas.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
-import org.springframework.data.annotation.Id;
+
 
 @Data // Anotação do Lombok que cria getters, setters, toString, etc. automaticamente.
 @Entity
@@ -13,6 +14,7 @@ public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String description;
     private String priority;
     private boolean completed;
